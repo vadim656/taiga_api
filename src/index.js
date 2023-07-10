3b29ec0282ae271c6237fae3e0d3dcc7da0d4963
foxsis <va.four2016@gmail.com>
2023-07-10 15:09:32 +0300

    fix json2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register ({ strapi }) {
+  register({ strapi }) {
     const extensionService = strapi.plugin('graphql').service('extension');
     extensionService.use(({ nexus }) => ({
       types: [
@@ -15,10 +15,10 @@ module.exports = {
           type: 'UsersPermissionsMe',
           definition(t) {
             t.string("calendar"),
-            t.string("user_type")
-            t.string("FIO"),
-            t.string("Zapisi"),
-            t.field('user_statuses', { type: 'UserStatusEntityResponse' });
+              t.string("user_type"),
+              t.string("FIO"),
+              t.string("Zapisi"),
+              t.field('user_statuses', { type: 'UserStatusEntityResponse' });
           },
         }),
       ]
@@ -32,5 +32,5 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) { },
 };
